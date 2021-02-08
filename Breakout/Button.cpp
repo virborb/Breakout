b9c26a79ce52bf0fbf408d8658c88401e464a694
Victor Lundgren <victor.lundgren@outlook.com>
2021-02-08 12:26:29 +0100

    Added doxygen to Button class

--- Breakout/Button.cpp
+++ Breakout/Button.cpp
@@ -1,5 +1,8 @@
 #include "Button.h"
 
+/**
+ * @brief Creates a new button.
+*/
 Button::Button()
 {
     rect.x = 0;
@@ -9,27 +12,51 @@ Button::Button()
     color = { 0x0, 0x0, 0xFF };
 }
 
+/**
+ * @brief Sets the position of the button.
+ * @param x the x cordinate.
+ * @param y the y cordinate.
+*/
 void Button::setPosition(int x, int y)
 {
     rect.x = x;
     rect.y = y;
 }
 
+/**
+ * @brief Gets the width of the button.
+ * @return The width.
+*/
 int Button::getWidth()
 {
     return rect.w;
 }
 
+/**
+ * @brief Gets the height of the button.
+ * @return the height.
+*/
 int Button::getHeight()
 {
     return rect.h;
 }
 
+/**
+ * @brief Sets the text on the button
+ * @param str The string.
+ * @param textColor the color of the text
+ * @param renderer The renderer to use.
+*/
 void Button::setText(std::string str, SDL_Color textColor, SDL_Renderer* renderer)
 {
     text.loadFromRenderedText(str, textColor, renderer);
 }
 
+/**
+ * @brief Handle if a button is pressed.
+ * @param e The SDL event pointer.
+ * @return true if button is pressed otherwise false.
+*/
 bool Button::handleEvent(SDL_Event* e)
 {
     if (e->button.button == SDL_BUTTON_LEFT)
@@ -45,6 +72,10 @@ bool Button::handleEvent(SDL_Event* e)
     return false;
 }
 
+/**
+ * @brief Render the button.
+ * @param renderer the renderer to use.
+*/
 void Button::render(SDL_Renderer* renderer)
 {
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
